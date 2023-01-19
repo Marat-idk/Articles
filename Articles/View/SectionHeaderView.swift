29dd5e7eede6cdee16cc1c7c94172b15a1a69be0
Marat-idk <molodoy.dracon@gmail.com>
2023-01-19 10:02:14 +0300

    added .gitignore

---
+++ Articles/View/SectionHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  SectionHeaderView.swift
+//  Articles
+//
+//  Created by Maxim Raskevich on 19.01.2023.
+//
+
+import UIKit
+
+class SectionHeaderView: UICollectionReusableView {
+        
+}
